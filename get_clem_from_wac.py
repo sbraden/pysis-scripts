efb5aa99e824058e2729427319ce6f14f49017a7
Sarah Braden <sebraden@asu.edu>
2013-06-20 08:14:21 -0700

    changes, ready for testing

--- get_clem_from_wac.py
+++ get_clem_from_wac.py
@@ -14,7 +14,6 @@ clem_dir = '/home/sbraden/Datasets/clementine/'
 projection = 'equirectangular'
 scale = 473.8 # units of meters per pixel
 
-# ext_len = -len('.IMG')
 
 def read_in_list(filename):
     with open(filename) as f:
@@ -22,69 +21,82 @@ def read_in_list(filename):
     return lines
 
 
-    output = isis.campt.check_output(from_=img_name)
-    output = content_re.search(output).group(1) 
-    phase_angle = parse_label(output)['GroundPoint']['Phase']
-    
-
-def get_pixel_scale(img_name):
-    output = isis.campt.check_output(from_=img_name)
-    output = content_re.search(output).group(1) 
-    pixel_scale = parse_label(output)['GroundPoint']['SampleResolution']
-    return minlat, maxlat, minlon, maxlon
-
-
-def isis.map2map(from_=rolo_name, to=rolodir + '/footprints/' + no_ext + '.' + rolo_name, 
-                    map=img_name+'.map', pixres='mpp', 
-                    resolution=7000, interp='nearestneighbor', 
-                    defaultrange='map')
-
-def create_maptemplate(region, feature):
+def create_maptemplate(region, projection, scale):
     '''
     Uses a set of latitude and longitude boundaries, a projection, and
     a scale to create a mapfile.
     region = (minlat, maxlat, minlon, maxlon)
     ''' 
-
-    minlat = str(region[0])
-    maxlat = str(region[1])
-    minlon = str(region[2])
-    maxlon = str(region[3])
-    clon = str( region[2] + abs(region[3]-region[2]) )
-    clat = str( region[0] + abs(region[1]-region[0]) )
+    # TODO: does it have to be a string now? I don't think so
+    center_longitude = region[2] + abs(region[3]-region[2])
+    center_latitude = region[0] + abs(region[1]-region[0])
 
     isis.maptemplate(
         projection_=projection,
         map  = 'temp_map.map', 
         clat = center_latitude,
         clon = center_longitude,
-        minlat = minimum_latitude,
-        maxlat = maximum_latitude,
-        minlon = minimum_longitude,
-        maxlon = maximum_longitude,
-        rngopt = user,
-        resopt = mpp,
+        minlat = region[0],
+        maxlat = region[1],
+        minlon = region[2],
+        maxlon = region[3],
+        rngopt = 'user',
+        resopt = 'mpp',
         resolution = scale
                     )
 
 
-def 
+def get_max_sample_line(img_name): # add to module
+    '''
+    Get the max sample line without reading in the image.
+    '''
+    output = isis.catlab.check_output(from_=img_name)
+    output = content_re.search(output).group(1)  # will this work?
+    max_sample = parse_label(output)['Dimensions']['Samples']
+    max_line = parse_label(output)['Dimensions']['Lines']
+    return max_sample, max_line
 
-    isis.maptemplate(map='D_mdis_wac_eqr.map', projection='equirectangular',
-        clon=0.0, clat=0.0, resopt='mpp', resolution=1000, rngopt='user', 
-        minlat=-40.0, maxlat=40.0, minlon=0.0, maxlon=360.0)
+
+def get_lat_lon_from_x_y(xy, img_name): # add to  module
+    '''
+    Takes xy tuple containing sample and line ints
+    Returns latitude and longitude 
+    '''
+    output = isis.mappt.check_output(
+        from_  = img_name, 
+        format = 'flat', 
+        type   = 'image', 
+        sample = xy(1), 
+        line   = xy(2)
+        )
+
+    output = content_re.search(output).group(1)  # will this work?
+    latitude = parse_label(output)['Results']['PlanetographicLatitude']
+    longitude = parse_label(output)['Results']['PositiveEast360Longitude']
+    return latitude, longitude
 
 
 def main():
 
-    images = iglob(source_dir + '*.cub')
+    image_list = read_in_list('temp.txt') # read in image list
 
-    # read in images
-    # extract corner sample, line 
-    # get min max lat lon 
-    # make new maptemplate
-    # make new cub file
-
+    for img_name in image_list:
+        # extract corner sample, line
+        max_xy = get_max_sample_line(img_name)
+        min_xy = (1,1)
+        # get min max lat lon 
+        min_latitude, max_longitude = get_lat_lon_from_x_y(max_xy, img_name)
+        max_latitude, min_longitude = get_lat_lon_from_x_y(min_xy, img_name)
+        region = (min_latitude, max_latitude, min_longitude, max_longitude)
+        # make new maptemplate
+        create_maptemplate(region, projection, scale)
+        # make new cub file
+        isis.map2map(
+            from_    = img_name, 
+            to       = img_name[-4]+'_clem.cub', 
+            map      = 'temp_map.map', 
+            matchmap = 'true'
+            )
 
 
 if __name__ == '__main__':
