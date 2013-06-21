ed254e35624ae4a341a3debe925cf32a2ff0d1aa
Sarah Braden <sebraden@asu.edu>
2013-06-20 20:15:31 -0700

    debug

--- cube_to_cloudplot.py
+++ cube_to_cloudplot.py
@@ -73,7 +73,7 @@ def make_cross_plot(wac_df, clm_df):
         roi_name = index_name[:-4]
         x = wac_df.loc[index_name].values
         y = clm_df.loc[roi_name+'_clm'].values
-        print mean(x[0]) # debug
+        print np.mean(x[0]) # debug
         plt.errorbar(x.mean(), y.mean(), xerr=np.std(x), yerr=np.std(y),
             marker='o', label=(roi_name), c=colour_circle.next())
 
