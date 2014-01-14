f282abe1512bee99bd2288bdfee4cb9407ad245c
Sarah Braden <sebraden@asu.edu>
2014-01-13 17:19:38 -0700

    debug

--- cube_to_cloudplot.py
+++ cube_to_cloudplot.py
@@ -20,8 +20,8 @@ from matplotlib import pyplot as plt
 from matplotlib.font_manager import FontProperties
 
 # on martini.local
-source_dir = '/home/sbraden/lunar_rois/matching_cubes/'
-#source_dir = '/home/sbraden/400mpp_coreg_wac_clm/'
+#source_dir = '/home/sbraden/lunar_rois/matching_cubes/'
+source_dir = '/home/sbraden/400mpp_coreg_wac_clm/'
 
 # Colors will be applyed to filters by filter in alphabetical ordear
 colors = [
