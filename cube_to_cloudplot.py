86f8b936045d082b14e006b0f90326b15046e721
Sarah Braden <sebraden@asu.edu>
2014-01-14 17:06:51 -0700

    debug

--- cube_to_cloudplot.py
+++ cube_to_cloudplot.py
@@ -21,7 +21,7 @@ from matplotlib.font_manager import FontProperties
 
 # on martini.local
 #source_dir = '/home/sbraden/lunar_rois/matching_cubes/'
-source_dir = '/home/sbraden/400mpp_coreg_wac_clm/'
+source_dir = '/home/sbraden/400mpp_immaturemare_6x6/'
 
 x1= 320
 x2= 415
